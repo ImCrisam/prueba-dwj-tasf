a9ddb6ab304e7ed06b6a3b77045d676d951ebee9
Imcrisam <Imcrisam@gmail.com>
2022-12-27 14:45:58 -0500

    init

---
+++ src/main/java/com/tasf/PruebaDwjTasfApplication.java
@@ -0,0 +1,13 @@
+package com.tasf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDwjTasfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDwjTasfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasf/PruebaDwjTasfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDwjTasfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
